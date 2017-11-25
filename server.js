14b773f4cab1bf7da468f413365aee9cb7a990c1
felix.ng <nfelix@gmail.com>
2017-11-25 11:04:26 +0000

    removing scope check for now

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const checkJwt = jwt({
   algorithms: ['RS256']
 });
 
-const checkScopes = jwtAuthz([ 'read:messages' ]);
+const checkScopes = jwtAuthz([]);
 
 app.get('/api/public', function(req, res) {
   res.json({ message: "Hello from a public endpoint! You don't need to be authenticated to see this." });
